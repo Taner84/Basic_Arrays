4b6e9b8a8368e2fef09f0d2e5529d6c700d837f8
Taner <taner@taner-k.nl>
2024-03-02 08:36:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Basic Arrays
+const groceries = ['Bananas', 'Apples', 'Oranges', ':Pears']
+//console.log(groceries)
+
+//grab the first index
+//console.log(groceries[0])
+//grab the third index
+//console.log(groceries[2])
+
+//Array methods
+groceries.push('cookies')
+//console.log(groceries)
+
+groceries.push('blueberries')
+
+//slice method
+console.log(groceries)
+// console.log(groceries.slice(3, 6))
+
+// indexOf method
+//console.log(groceries.indexOf('Oranges'))
+
+//.length method
+console.log(groceries.length)
